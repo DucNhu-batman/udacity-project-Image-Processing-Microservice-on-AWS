aa43738d2e0d3bd2e962827091ae89768c0e66b0
DESKTOP-02B3E8I\AD <ducnhu2k1@gmail.com>
2024-04-11 20:29:34 +0700

    udpate logic to show image

--- server.js
+++ server.js
@@ -37,15 +37,23 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
     res.send("try GET /filteredimage?image_url=https://mcdn.coolmate.me/image/October2023/nhan-vat-doraemon-3012_329.jpg")
   } );
 
-  app.get( "/filteredimage", async (req, res) => {
-    const prm = filterImageFromURL(req.query.image_url);
-    prm.then(val => {
-        res.send(val)
-    })
-    prm.catch((err) => {
-      res.send(`Error: ${err}`)
-    })
-  } );
+  app.get( "/filteredimage", async(req, res) => {
+    const {image_url} = req.query;
+    if(!image_url) {
+      resolve({
+        status: 0,
+        error: "Dont have any img link, please give for me a url!!"
+      });
+    } else {
+      await filterImageFromURL(image_url).then( function (image_filtered_path){
+        res.sendFile(image_filtered_path, () => {       
+          deleteLocalFiles([image_filtered_path]);       
+        });   
+      }).catch(function(err){
+        res.status(400).send('Please make sure that the image url is valid!');
+      });
+    }
+  });
   
 
   // Start the Server
--- util/util.js
+++ util/util.js
@@ -4,26 +4,15 @@ import Jimp from "jimp";
  export async function filterImageFromURL(inputURL) {
   return new Promise(async (resolve, reject) => {
     try {
-      if(!inputURL) {
-        resolve({
-          status: 0,
-          error: "Dont have any img link, please give for me a url!!"
-        });
-        return
-      }
-
       const photo = await Jimp.read(inputURL);
       const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+        "/tmp/vcl." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
         .resize(256, 256) 
         .quality(60) 
         .greyscale() 
         .write(outpath, (img) => {
-          resolve({
-            status: 200,
-            info: "We have a good url: " + inputURL
-          });
+          resolve(outpath);
         });
     } catch (error) {
       resolve({
